c687a2ef410a62d8d7f4a2cc149e7aa25f91a120
minsea1256 <minsea1256@gmail.com>
2024-06-07 20:26:43 +0900

    240607 community/search

---
+++ resources/components/CommunityComponent.vue
@@ -0,0 +1,78 @@
+<template>
+        <main class="main-container">
+        <div class="nav-container">
+            <div class="nav-box">
+                <div class="page-title">
+                    <span>캠핑 Talk</span>
+                </div>
+                <div class="board-name-list">
+                    <div class="board-name">
+                        <p>가입인사</p>
+                        <p>자유 게시판</p>
+                        <p>캠핑꿀팁</p>
+                        <p>리뷰 게시판</p>
+                        <p>랭킹 게시판</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-container">
+            <div class="content-top">
+                <div class="board-select">
+                    <span>자유 게시판</span>
+                </div>
+                <div class="board-comment">
+                    <span>어쩌구 저쩌구를 남겨보세요.</span>
+                </div>
+                <div class="board-order-by">
+                    <span>최신순</span>
+                    <span>인기순</span>
+                </div>
+            </div>
+            <div class="content-bottom">
+                <div class="content-box">
+                    <div class="content-column">
+                        <span>글 번호</span>
+                        <span>글 제목</span>
+                        <span>작성자</span>
+                        <span>작성일</span>
+                        <span>조회수</span>
+                    </div>
+                    <hr>
+                    <div class="content-item">
+                        <span>10</span>
+                        <span>뭐라뭐라뭐라</span>
+                        <span>가나다</span>
+                        <span>2024-06-06</span>
+                        <span>66</span>
+                    </div>
+                    <hr class="item-hr">
+                    <div class="content-item">
+                        <span>10</span>
+                        <span>뭐라뭐라뭐라</span>
+                        <span>가나다</span>
+                        <span>2024-06-06</span>
+                        <span>66</span>
+                    </div>
+                    <hr class="item-hr">
+                    <div class="content-item">
+                        <span>10</span>
+                        <span>뭐라뭐라뭐라</span>
+                        <span>가나다</span>
+                        <span>2024-06-06</span>
+                        <span>66</span>
+                    </div>
+                    <hr class="item-hr">
+                </div>
+
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+</script>
+
+<style>
+/* @import url('/doc_bb/css/board.css'); */
+</style>
