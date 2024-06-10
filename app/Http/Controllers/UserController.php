47d005296c2c6941a5272b67567178ae6a38750f
lxxnee <seolin9787@nate.com>
2024-06-10 17:45:54 +0900

    20240610 이서린 nav바 고치기

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,58 +28,55 @@ class UserController extends Controller
         }
     }
 
+       // 회원가입 처리
     // 회원가입 처리
     public function register(Request $request)
     {
         $requestData = $request->all();
 
         $validator = validator::make(
-            $requestData
-            ,[
-                'id' => ['required','min:4','max:20','unique:users', 'regex:/^[a-zA-Z0-9]+$/']
-                ,'password' => ['required', 'min:2', 'max:20', 'regex:/^[a-zA-Z0-9!@*]+$/']
-                ,'ps_chk' => ['same:password']
-                ,'nickname' => ['required', 'min:2', 'max:20', 'regex:/^[가-힣]+$/u']
-                ,'name' => ['required', 'min:2', 'max:20', 'regex:/^[가-힣]+$/u']
-                ,'email' => ['required', 'regex:/^[-A-Za-z0-9_]+[-A-Za-z0-9_.][@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.][.]{1}[A-Za-z]{1,5}$/']
-                ,'phone_number' => ['required', 'regex:/^\d{2,3}-\d{3,4}-\d{4}$/']
+            $requestData,
+            [
+                'password' => ['required', 'min:2', 'max:20', 'regex:/^[a-zA-Z0-9!@*]+$/'],
+                'ps_chk' => ['same:password'],
+                'nick_name' => ['required', 'min:2', 'max:10', 'regex:/^[가-힣]+$/u'],
+                'name' => ['required', 'min:2', 'max:20', 'regex:/^[가-힣]+$/u'],
+                'email' => ['required', 'regex:/^[-A-Za-z0-9_]+[-A-Za-z0-9_.][@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.][.]{1}[A-Za-z]{1,5}$/'],
+                'tel' => ['required', 'regex:/^(\d{2,3}-\d{3,4}-\d{4})|(\d{10,11})$/']
             ]
         );
 
-        // 유효성 검사 실패 체크
-        if($validator->fails()) {
+        if ($validator->fails()) {
             Log::debug('유효성 검사 실패', $validator->errors()->toArray());
             throw new MyValidateException('E01');
         }
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
 
         // 작성 데이터 생성
-    $insertData = $request->all();
+        $insertData = $request->all();
 
-    // 비밀번호 설정
-    $insertData['password'] = Hash::make($request->password);
+        // 데이터 로그 출력
+        Log::debug('Insert data: ', $insertData);
 
-    // 인서트 처리
-    $userInfo = User::create($insertData);
+        // 비밀번호 설정
+        $insertData['password'] = Hash::make($request->password);
 
-    $responseData = [
-        'code' => '0'
-        ,'msg' => '회원가입 성공'
-        ,'data' => $userInfo
-    ];
+        // 인서트 처리
+        $userInfo = User::create($insertData);
 
-    return response()->json($responseData, 200);
+        $responseData = [
+            'code' => '0',
+            'msg' => '회원가입 성공',
+            'data' => $userInfo
+        ];
 
+        return response()->json($responseData, 200);
     }
 
-    // 로그아웃 처리
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json(['message' => '로그아웃 성공']);
-    }
+      // 로그아웃 처리
+      public function logout()
+      {
+          Auth::logout();
+          return response()->json(['message' => '로그아웃 성공']);
+      }
+  
 }
