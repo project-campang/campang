9ba97c4640d05b898c535183aa5a99c89f611092
leenara02 <nala016@naver.com>
2024-06-14 17:23:34 +0900

    240614 이나라 댓글완료

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -42,26 +42,29 @@ class CommentController extends Controller
     }
 
     // 댓글 획득
-    public function commentGet() {
-        $commentData = Comment::select('comments.*', 'users.nick_name')
-                                ->join('users','users.id','=','comments.user_id')
-                                ->orderBy('comments.id', 'DESC')
-                                ->get();
-        $responseData = [
-            'code' => '0',
-            'msg' => '댓글 획득 완료',
-            'data' => $commentData->toArray()
-            ];
-        // Log::debug('쿼리 작동됌', $commentData->toArray());
-        // Log::debug('responseData', $responseData);
-        // Log::debug('리턴');
-        return response()->json($responseData, 200);
-    }
+    // public function commentGet() {
+    //     $commentData = Comment::select('comments.*', 'users.nick_name')
+    //                             ->join('users','users.id','=','comments.user_id')
+    //                             ->orderBy('comments.id', 'DESC')
+    //                             ->get();
+    //     $responseData = [
+    //         'code' => '0',
+    //         'msg' => '댓글 획득 완료',
+    //         'data' => $commentData->toArray()
+    //         ];
+    //     Log::debug('쿼리 작동됌', $commentData->toArray());
+    //     // Log::debug('responseData', $responseData);
+    //     // Log::debug('리턴');
+    //     return response()->json($responseData, 200);
+    // }
 
     // 댓글 페이지네이션
     public function commentPaginate() {
-
-        $comment = Comment::latest()->paginate(5);
+        $comment = Comment::select('comments.*', 'users.nick_name')
+                                ->join('users','users.id','=','comments.user_id')
+                                ->orderBy('comments.id', 'DESC')
+                                ->paginate(5);
+        // $comment = Comment::latest()->paginate(5);
         Log::debug("comment", $comment->toArray());
 
         $responseData = [
--- resources/components/CommentCreate.vue
+++ resources/components/CommentCreate.vue
@@ -2,14 +2,23 @@
     <div>
         <hr>
         <form class="comment-form" id="commentForm">
-            <textarea class="comment-area" name="comment" max="1000" placeholder="댓글을 남겨보세요"></textarea>
-            <button @click="$store.dispatch('commentStore')" type="button" class="submit-btn">작성</button>
+            <textarea v-model="newComment" class="comment-area" name="comment" max="1000" placeholder="댓글을 남겨보세요"></textarea>
+            <button @click="commentEvent()" type="button" class="submit-btn">작성</button>
             <button type="reset" class="reset">초기화</button>
         </form>
     </div>
 </template>
 
 <script setup>
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+
+function commentEvent(){
+    store.dispatch('commentStore');
+    window.location.reload();
+}
 
 </script>
 
